d72b661ef45950ffb77af7f16a11b55691b6a177
tarosio <tkm7160@icloud.com>
2025-06-27 11:48:13 +0900

    Upload files
    公開用のファイルをアップロード

---
+++ js/main.js
@@ -0,0 +1,84 @@
+
+// GSAPから,メディアクエリの関数を実行
+const mm = gsap.matchMedia()
+// ブレイクポイント指定
+// 端末の画面幅が1025px以上の時（pc）
+mm.add("(min-width: 1025px" , () => {
+    gsap.fromTo(
+    ".concept-first .column-left",
+    2,
+    {
+        x:-100,
+        opacity:0
+    },
+    {
+        x:0,
+        opacity:1,
+        scrollTrigger: {
+            trigger:".concept-first .column-left",
+            start:"top center",
+            markers:true
+        }
+    }
+)
+
+gsap.fromTo(
+    ".concept-first .column-right",
+    2,
+    {
+        x:100,
+        opacity:0,
+    },
+    {
+        x:0,
+        delay:0.5,
+        opacity:1,
+        scrollTrigger: {
+            trigger:".concept-first .column-right",
+            start:"top center",
+            markers:true
+        }
+    }
+)
+
+})
+// 端末の画面幅が599px以上の時（スマホ）
+mm.add("(max-width: 599px" , () => {
+    gsap.fromTo(
+    ".concept-first .column-left",
+    2,
+    {
+        y:-100,
+        opacity:0
+    },
+    {
+        y:0,
+        opacity:1,
+        scrollTrigger: {
+            trigger:".concept-first .column-left",
+            start:"top center",
+            markers:true
+        }
+    }
+)
+
+gsap.fromTo(
+    ".concept-first .column-right",
+    2,
+    {
+        y:100,
+        opacity:0,
+    },
+    {
+        y:0,
+        delay:0.5,
+        opacity:1,
+        scrollTrigger: {
+            trigger:".concept-first .column-right",
+            start:"top center",
+            markers:true
+        }
+    }
+)
+
+})
